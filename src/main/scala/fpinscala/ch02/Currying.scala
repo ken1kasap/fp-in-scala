3a624f40d887a462e48004054571359620f52f4b
ken1kasap <ken1kasap@gmail.com>
2017-12-31 22:48:29 +0900

    Exercise 2.4

--- src/main/scala/fpinscala/ch02/Currying.scala
+++ src/main/scala/fpinscala/ch02/Currying.scala
@@ -4,4 +4,8 @@ object Currying {
   // Exercise 2.3
   def curry[A, B, C](f: (A, B) => C): A => (B => C) =
     (a: A) => (b: B) => f(a, b)
+
+  // Exercise 2.4. uncurryを実装する
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a: A, b: B) => f(a)(b)
 }
