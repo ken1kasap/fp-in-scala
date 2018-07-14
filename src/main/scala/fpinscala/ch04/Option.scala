84076dceec3c1e23238f655125600402584b9207
ken1kasap <ken1kasap@gmail.com>
2018-07-15 01:02:57 +0900

    add mean function.

--- src/main/scala/fpinscala/ch04/Option.scala
+++ src/main/scala/fpinscala/ch04/Option.scala
@@ -27,3 +27,9 @@ sealed trait Option[+A] {
 
 case class Some[+A](get: A) extends Option[A]
 case object None            extends Option[Nothing]
+
+object Option {
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+}
