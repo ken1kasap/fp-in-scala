b11a8c18064a40fbdb5a5d4e8e7fcf16b31851e8
ken1kasap <ken1kasap@gmail.com>
2018-07-15 00:51:58 +0900

    Exercise 4.1

--- src/main/scala/fpinscala/ch04/Option.scala
+++ src/main/scala/fpinscala/ch04/Option.scala
@@ -1,5 +1,29 @@
 package fpinscala.ch04
 
-sealed trait Option[+A]
+sealed trait Option[+A] {
+  // Exercise 4.1
+  def map[B](f: A => B): Option[B] = this match {
+    case None    => None
+    case Some(a) => Some(f(a))
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] = map(f) getOrElse None
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case Some(a) => a
+    case None    => default
+  }
+
+  def orElse[B >: A](ob: Option[B]): Option[B] = this match {
+    case None => ob
+    case _    => this
+  }
+
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(a) if f(a) => this
+    case _               => None
+  }
+}
+
 case class Some[+A](get: A) extends Option[A]
 case object None            extends Option[Nothing]
