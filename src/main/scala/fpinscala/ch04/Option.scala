92b69e31de638066c5572ee2bb12037de571512d
ken1kasap <ken1kasap@gmail.com>
2018-07-15 01:21:00 +0900

    Exercise 4.2

--- src/main/scala/fpinscala/ch04/Option.scala
+++ src/main/scala/fpinscala/ch04/Option.scala
@@ -32,4 +32,12 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
+
+  // Exercise 4.2
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs) flatMap { m =>
+      mean(xs.map { x =>
+        math.pow(x - m, 2)
+      })
+    }
 }
