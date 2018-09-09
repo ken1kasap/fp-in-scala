b83a335f64b2692bff13e12a060d5572bc7295ad
ken1kasap <ken1kasap@gmail.com>
2018-09-10 00:00:53 +0900

    re-format.

--- src/main/scala/fpinscala/ch04/Either.scala
+++ src/main/scala/fpinscala/ch04/Either.scala
@@ -1,20 +1,23 @@
 package fpinscala.ch04
 
 sealed trait Either[+E, +A] {
-  def map[B](f: A => B): Either[E, B] = this match {
-    case Left(e)  => Left(e)
-    case Right(a) => Right(f(a))
-  }
+  def map[B](f: A => B): Either[E, B] =
+    this match {
+      case Left(e)  => Left(e)
+      case Right(a) => Right(f(a))
+    }
 
-  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
-    case Left(e)  => Left(e)
-    case Right(a) => f(a)
-  }
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] =
+    this match {
+      case Left(e)  => Left(e)
+      case Right(a) => f(a)
+    }
 
-  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
-    case Left(_)  => b
-    case Right(a) => Right(a)
-  }
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] =
+    this match {
+      case Left(_)  => b
+      case Right(a) => Right(a)
+    }
 
   def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
     for {
