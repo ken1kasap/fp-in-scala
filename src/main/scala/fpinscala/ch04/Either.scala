2f765197cfcf25d046e5a1d195ab34216614d5f7
ken1kasap <ken1kasap@gmail.com>
2018-09-09 20:00:27 +0900

    add Try.

--- src/main/scala/fpinscala/ch04/Either.scala
+++ src/main/scala/fpinscala/ch04/Either.scala
@@ -15,4 +15,8 @@ object Either {
   def saveDiv(x: Int, y: Int): Either[Exception, Int] =
     try Right(x / y)
     catch { case e: Exception => Left(e) }
+
+  def Try[A](a: => A): Either[Exception, A] =
+    try Right(a)
+    catch { case e: Exception => Left(e) }
 }
