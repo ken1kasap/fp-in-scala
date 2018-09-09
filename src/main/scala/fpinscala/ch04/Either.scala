7844f19f60f7f5f03b97ae40324ace5529e8e58d
ken1kasap <ken1kasap@gmail.com>
2018-09-09 22:37:58 +0900

    implement map, flatMap, orElse and map2.

--- src/main/scala/fpinscala/ch04/Either.scala
+++ src/main/scala/fpinscala/ch04/Either.scala
@@ -1,6 +1,28 @@
 package fpinscala.ch04
 
-sealed trait Either[+E, +A]
+sealed trait Either[+E, +A] {
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Left(e)  => Left(e)
+    case Right(a) => Right(f(a))
+  }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Left(e)  => Left(e)
+    case Right(a) => f(a)
+  }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(_)  => b
+    case Right(a) => Right(a)
+  }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for {
+      a  <- this
+      bb <- b
+    } yield f(a, bb)
+}
+
 case class Left[+E](value: E)  extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
 
